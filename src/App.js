e9055bfa4d705af8cee69c960ce1d1ff62355e60
kimjeonghoonn <jhun6241@daum.net>
2022-10-17 15:31:46 +0900

    TodoForm Submit event test ok

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import TodoFrom from './TodoForm';
 
 function App() {
   return (
     <div className="App">
+      <TodoFrom />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
