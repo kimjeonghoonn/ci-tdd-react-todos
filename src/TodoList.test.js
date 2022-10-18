cf8acd978d59b9c6e6f0f7fc9ca133e81691fee7
kimjeonghoonn <jhun6241@daum.net>
2022-10-18 10:07:11 +0900

    TodoList TodoItem onToggle onRemove test ok

--- src/TodoList.test.js
+++ src/TodoList.test.js
@@ -19,6 +19,16 @@ describe('<TodoList />', () => {
         const { getByText } = render(<TodoList todos={sampleTodos} />);
         getByText(sampleTodos[0].text);
         getByText(sampleTodos[1].text);
+    });
 
-    })
+    it('calls onToggle and onRemove', () => {
+        const onToggle = jest.fn();
+        const onRemove = jest.fn();
+        const {getByText, getAllByText} = render(<TodoList todos={sampleTodos}
+            onToggle={onToggle} onRemove={onRemove} />);
+        fireEvent.click(getByText(sampleTodos[0].text));
+        expect(onToggle).toBeCalledWith(sampleTodos[0].id);
+        fireEvent.click(getAllByText('삭제')[0]);
+        expect(onRemove).toBeCalledWith(sampleTodos[0].id);
+    });
 });
